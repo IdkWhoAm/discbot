5dfd048c33e1e62096b094a300c613dbefa3610b
IdkWhoAm <43629881+IdkWhoAm@users.noreply.github.com>
2019-05-07 06:30:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,42 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+bot.on('ready', () => {
+    console.log('Hello World!');
+	 bot.user.setStatus('available')
+    bot.user.setPresence({
+        game: {
+            name: 'на тебя',
+            type: "WATCHING",
+        }
+    });
 });
-
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
+    if (message.author.bot === true) return;
+	if (message.content === ";verify") {
+		message.delete()
+		if (!message.channel.name === "потверждение") return;
+		message.channel.send('Потверждаю...').then(msg => {
+			msg.delete(6)
+		})
+		let sor = message.guild.roles.find('name','непотвержден')
+		let ser = message.guild.roles.find('name','Игрок')
+		message.guild.member(message.author).removeRole(sor)
+		message.guild.member(message.author).addRole(ser).then(() => {
+			message.channel.send('Потверждены!').then(msg => {
+				msg.delete(4)
+			})
+		} ).catch(err => {
+			message.channel.send('Че-то пошло не так').then(msg => {
+				msg.delete(4)
+			})
+		})
+	}
+	if (message.content.substring(0,4) == ";say") {
+		if (!message.author === "NikitaPlays19") return;
+		message.delete()
+		message.channel.send(message.content.substring(5))
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
