6204b841a8111b06a2d485601bdb8e71896b21bd
IdkWhoAm <43629881+IdkWhoAm@users.noreply.github.com>
2019-05-07 06:52:17 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -3,26 +3,18 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setStatus('available');
-    client.user.setPresence({
-        game: {
-            name: 'на тебя',
-            type: "WATCHING",
-        }
-    });
 });
 
 client.on('message', message => {
-    if (message.author.bot === true) return;
     if (message.content === 'ping') {
     	message.channel.send('PONG!');
-    }
+  	}
 });
 
 client.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
-   }
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
