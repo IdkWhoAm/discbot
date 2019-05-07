25332ce94cf4855f1e59df77e15df391f1f0c439
IdkWhoAm <43629881+IdkWhoAm@users.noreply.github.com>
2019-05-07 20:11:46 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js')
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log('Hello World!');
+	 client.user.setStatus('available');
+    client.user.setPresence({
+        game: {
+            name: 'на тебя',
+            type: "PLAYING",
+        }
+    });
+});
+client.on('message', message => {
+	if (message.author.bot === true) return;
+	if (message.content === ";verify") {
+		message.delete()
+		if (!message.channel.name === "потверждение") return;
+		message.channel.send('Потверждаю...').then(msg => {
+			msg.delete(6)
+		})
+		let sor = message.guild.roles.find('name','непотвержден')
+		let ser = message.guild.roles.find('name','Игрок')
+		message.guild.member(message.author).removeRole(sor)
+		message.guild.member(message.author).addRole(ser).then(() => {
+			message.channel.send('Потверждены!').then(msg => {
+				msg.delete(4)
+			})
+		} ).catch(err => {
+			message.channel.send('Че-то пошло не так').then(msg => {
+				msg.delete(4)
+			})
+		})
+	}
+	if (message.content.substring(0,4) == ";say") {
+		if (!message.author === "NikitaPlays19") return;
+		message.delete()
+		message.channel.send(message.content.substring(5))
+	};
+	if (message.content.substring(0,5) == ";kick") {
+		let iqo = message.guild.roles.find("name","Admin");
+		if (!message.member.roles.has(iqo.id) return message.channel.send('Вам не разрешено использовать эту команду.');
+		message.channel.send('Окей, ща попробую...');
+		if (!message.mentions.users.first()) return message.channel.send('Эй, я не опознал этого игрока!');
+		if (!message.guild.member(message.mentions.users.first()) return message.channel.send('Но он не в нашем сервере!');
+		message.guild.member(message.mentions.users.first()).kick('Плохо себя ввел...').then(() => {
+			message.channel.send('Ура! Я смог кикнуть ${user.tag}!')
+		}).catch(err => {
+			message.channel.send('Ух, чет пошло не так')
+		})
+	}
+});
+client.on('guildMemberAdd', member => {
+	const channel = member.guild.channels.find(ch => ch.name === 'логи');
+	if (!channel) return;
+	channel.send('Знаете кто к нам присоединился? Это ${member}!');
+});
+client.on('guildMemberRemove', member => {
+	const channel = member.guild.channels.find(ch => ch.name === 'логи');
+	if (!channel) return;
+	channel.send('Ох, от нас ушел ${member}... Надеюсь, мы тебя встретим когда нибудь...');
+});
+client.login(process.env.BOT_TOKEN) 
