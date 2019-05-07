19ef733ba40517b9fd9a92b39afd7e02a3543386
IdkWhoAm <43629881+IdkWhoAm@users.noreply.github.com>
2019-05-07 06:45:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,13 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    bot.user.setStatus('available')
+    bot.user.setPresence({
+        game: {
+            name: 'на тебя',
+            type: "WATCHING",
+        }
+    });
 });
 
 client.on('message', message => {
